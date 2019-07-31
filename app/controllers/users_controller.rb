03c1678f60f9719da33a05e4b7be1662246da0fd
Ratsamee <jayin@hotmail.com>
2019-07-31 10:01:25 +1000

    cleaned user's code and fixed login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,17 +20,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    # user_id = params[:id]
-    # if (!@current_user.nil? && !@current_user.admin?)
-    #   user_id = @current_user.id
-    # end
-    user_id = (!@current_user.nil? && !@current_user.admin?)? @current_user.id  : params[:id]
-    @user = User.find_by :id => user_id
+    @user = User.find_by :id => get_user_id
   end
 
   def update
-    user_id = (!@current_user.nil? && !@current_user.admin?)? @current_user.id  : params[:id]
-    @user = User.find_by :id => user_id
+    @user = User.find_by :id => get_user_id
     if @user.update user_params
       redirect_to @user
     else
@@ -40,13 +34,12 @@ class UsersController < ApplicationController
   end
 
   def show
-    user_id = (!@current_user.nil? && !@current_user.admin?)? @current_user.id  : params[:id]
-    @user = User.find_by :id => user_id
+    @user = User.find_by :id => get_user_id
   end
 
   def destroy
-    user_id = (!@current_user.nil? && !@current_user.admin?)? @current_user.id  : params[:id]
-    user = User.find_by :id => user_id
+    
+    user = User.find_by :id => get_user_id
     user.destroy
     redirect_to users_path
   end
@@ -54,4 +47,9 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
+
+  def get_user_id
+    (!@current_user.nil? && !@current_user.admin?)? @current_user.id  : params[:id]
+  end
+
 end
